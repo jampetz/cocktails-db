617c746a20e1cab0e33cdd816bf5a7a51e69f9ef
jampetz <fx314@pm.me>
2021-11-23 23:11:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cocktails DB
+
+## [Live instance](https://jam-cocktails-db.netlify.app/)
+
+### Features:
+
+- Done with help of [Free API](https://www.thecocktaildb.com/api.php);
+- useState;
+- useEffect;
+- React Router;
+- useCallback;
+- useContext;
+- useRef.
+
+### Initializing local instance
+
+```
+cd cocktails-db && npm install && npm start
+```
+
+### Building
+
+```
+cd cocktails-db && npm run build
+```
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Alcohol has been produced and consumed by humans for its psychoactive
+        effects for almost 10,000 years. Drinking alcohol is generally socially
+        acceptable and is legal in most countries, unlike with many other
+        recreational substances. However, there are often restrictions on
+        alcohol sale and use, for instance a minimum age for drinking and laws
+        against public drinking and drinking and driving.
+      </p>
+    </section>
+  );
+};
+
+export default About;
